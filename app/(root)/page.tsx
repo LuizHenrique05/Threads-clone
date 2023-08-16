2f3fd32570c45e48e60123e9babb12d13cc1cd20
Luiz Henrique <henrique.luiz-sup@univem.edu.br>
2023-08-16 16:12:43 -0300

    feat: create clerk provider integration logic

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from '@clerk/nextjs'
+
 export default function Home() {
   return (
-    <main>
-      <h1>threads</h1>
-    </main>
+    <div>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
   )
 }
