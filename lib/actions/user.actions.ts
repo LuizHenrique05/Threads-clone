21b1d71133c370c94a77aea192c6c13b531b02cb
Luiz Henrique <henrique.luiz-sup@univem.edu.br>
2023-08-17 12:08:02 -0300

    feat: create backend users logic

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,32 @@
+'use server'
+
+import { revalidatePath } from "next/cache"
+import User from "../models/user.model"
+import { connectToDB } from "../mongoose"
+
+interface Params {
+    userId: string,
+    name: string,
+    username: string,
+    bio: string,
+    image: string,
+    path: string
+}
+
+export async function updateUser({userId, name, username, bio, image, path}: Params): Promise<void> {
+    connectToDB()
+
+    try {
+        await User.findOneAndUpdate({ id: userId }, {
+            username: username.toLocaleLowerCase(),
+            name,
+            bio,
+            image,
+            onboarded: true
+        }, { upsert: true })
+    
+        if (path === '/profile/edit') revalidatePath(path)
+    } catch (error: any) {
+        throw new Error(`Failed to create/update user: ${error.message}`)
+    }
+}
