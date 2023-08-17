517a355d8444cba69f79e00aed0afed035f00ceb
Luiz Henrique <henrique.luiz-sup@univem.edu.br>
2023-08-17 16:51:17 -0300

    feat: create thread page backend logic

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -29,8 +29,9 @@ export async function createThread({ text, author, communityId, path }: Params) 
     })
 
     revalidatePath(path)
-  } catch (error: any) {
-    throw new Error(`Failed to create thread: ${error.message}`)
+  } catch (err) {
+    console.log('Failed to create thread', err)
+    throw new Error('Failed to create thread')
   }
 }
 
@@ -50,16 +51,82 @@ export async function fetchThreads(pageNumber = 1, pageSize = 20) {
         model: User,
         select: '_id name parentId image'
       }
-    });
+    })
 
   // Count the total number of top-level threads (threads) i.e., threads that are not comments.
   const totalthreadsCount = await Thread.countDocuments({
     parentId: { $in: [null, undefined] },
-  }); // Get the total count of threads
+  })
 
   const threads = await threadsQuery.exec()
 
   const isNext = totalthreadsCount > skipAmount + threads.length
 
   return { threads, isNext }
+}
+
+export async function fetchThreadById(threadId: string) {
+  connectToDB()
+
+  try {
+    const thread = await Thread.findById(threadId)
+      .populate({
+        path: 'author',
+        model: User,
+        select: '_id id name image',
+      })
+      .populate({
+        path: 'children',
+        populate: [
+          {
+            path: 'author',
+            model: User,
+            select: '_id id name parentId image', 
+          },
+          {
+            path: 'children',
+            model: Thread, 
+            populate: {
+              path: 'author', 
+              model: User,
+              select: '_id id name parentId image', 
+            },
+          },
+        ],
+      })
+      .exec()
+
+    return thread
+  } catch (err) {
+    console.error('Error while fetching thread:', err)
+    throw new Error('Unable to fetch thread')
+  }
+}
+
+export async function addCommentToThread(threadId: string, commentText: string, userId: string, path: string ) {
+  connectToDB()
+
+  try {
+    const originalThread = await Thread.findById(threadId)
+
+    if (!originalThread) throw new Error('Thread not found')
+
+    const commentThread = new Thread({
+      text: commentText,
+      author: userId,
+      parentId: threadId, // Set the parentId to the original thread's ID
+    })
+
+    const savedCommentThread = await commentThread.save()
+
+    // Add the comment thread's ID to the original thread's children array
+    originalThread.children.push(savedCommentThread._id)
+
+    await originalThread.save()
+
+    revalidatePath(path)
+  } catch (err) {
+    console.error('Error while adding comment:', err)
+    throw new Error('Unable to add comment')
+  }
 }
