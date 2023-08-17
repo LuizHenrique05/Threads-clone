a6acf6d187a16a65eb40572b774c9ba8b289e232
Luiz Henrique <henrique.luiz-sup@univem.edu.br>
2023-08-17 16:10:12 -0300

    feat: create thread feed logic

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,36 @@
-import { UserButton } from '@clerk/nextjs'
+import ThreadCard from "@/components/cards/ThreadCard"
+import { fetchThreads } from "@/lib/actions/thread.actions"
+import { currentUser } from "@clerk/nextjs"
 
-export default function Home() {
+export default async function Home() {
+  const result = await fetchThreads(1, 30)
+  const user = await currentUser()
+
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
+
+      <div className="mt-9 flex flex-col gap-10">
+        {result.threads.length === 0 ? (
+          <p className="no-result">No threads found</p>
+        ) : (
+          <>
+            {result.threads.map((thread) => (
+              <ThreadCard
+                key={thread._id} 
+                id={thread._id} 
+                userId={user?.id || ''} 
+                parentId={thread.parentId} 
+                content={thread.text} 
+                author={thread.author} 
+                community={thread.community} 
+                createdAt={thread.createdAt}  
+                comments={thread.children}  
+              />
+            ))}
+          </>
+        )}
+      </div>
     </>
   )
 }
--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -33,3 +33,33 @@ export async function createThread({ text, author, communityId, path }: Params) 
     throw new Error(`Failed to create thread: ${error.message}`)
   }
 }
+
+export async function fetchThreads(pageNumber = 1, pageSize = 20) {
+  connectToDB()
+
+  const skipAmount = (pageNumber - 1) * pageSize
+
+  // Create a query to fetch the threads that have no parent (top-level threads) (a thread that is not a comment/reply).
+  const threadsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: 'desc' })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: 'author', model: User })
+    .populate({ path: 'children', populate: {
+        path: 'author',
+        model: User,
+        select: '_id name parentId image'
+      }
+    });
+
+  // Count the total number of top-level threads (threads) i.e., threads that are not comments.
+  const totalthreadsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  }); // Get the total count of threads
+
+  const threads = await threadsQuery.exec()
+
+  const isNext = totalthreadsCount > skipAmount + threads.length
+
+  return { threads, isNext }
+}
