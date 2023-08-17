33907a8b9a6a6e2abcbe5d7c7a637c7f92a9cc1a
Luiz Henrique <henrique.luiz-sup@univem.edu.br>
2023-08-17 17:50:35 -0300

    refactor: removing comments

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,13 +6,11 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-// created by chatgpt
 export function isBase64Image(imageData: string) {
   const base64Regex = /^data:image\/(png|jpe?g|gif|webp);base64,/;
   return base64Regex.test(imageData);
 }
 
-// created by chatgpt
 export function formatDateString(dateString: string) {
   const options: Intl.DateTimeFormatOptions = {
     year: "numeric",
@@ -31,7 +29,6 @@ export function formatDateString(dateString: string) {
   return `${time} - ${formattedDate}`;
 }
 
-// created by chatgpt
 export function formatThreadCount(count: number): string {
   if (count === 0) {
     return "No Threads";
