27343914de066c92282f858e96d66fa9384bcc06
Luiz Henrique <henrique.luiz-sup@univem.edu.br>
2023-08-17 10:28:57 -0300

    feat: create rightsidebar

---
+++ components/shareds/RightSidebar.tsx
@@ -0,0 +1,25 @@
+import { currentUser } from '@clerk/nextjs'
+
+async function RightSidebar() {
+  const user = await currentUser()
+  if (!user) return null
+
+  return (
+    <section className="custom-scrollbar rightsidebar">
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-heading4-medium text-light-1">
+          Suggested Communities
+        </h3>
+
+        <div className="mt-7 flex w-[350px] flex-col gap-9"></div>
+      </div>
+
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-heading4-medium text-light-1">Similar Minds</h3>
+        <div className="mt-7 flex w-[350px] flex-col gap-10"></div>
+      </div>
+    </section>
+  )
+}
+
+export default RightSidebar
