ce8b97888e73adf3d2b16b138dd61b73ed82e2ea
Luiz Henrique <henrique.luiz-sup@univem.edu.br>
2023-08-17 12:37:27 -0300

    feat: create fetchUser function

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,16 @@ import { revalidatePath } from "next/cache"
 import User from "../models/user.model"
 import { connectToDB } from "../mongoose"
 
+export async function fetchUser(userId: string) {
+    try {
+      connectToDB();
+  
+      return await User.findOne({ id: userId })
+    } catch (error: any) {
+      throw new Error(`Failed to fetch user: ${error.message}`);
+    }
+  }
+
 interface Params {
     userId: string,
     name: string,
