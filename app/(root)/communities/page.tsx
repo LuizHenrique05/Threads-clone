9c236b6e58a6bf78d28e637317fe7ad3b6513c7f
Luiz Henrique <henrique.luiz-sup@univem.edu.br>
2023-08-17 22:25:39 -0300

    feat: create community frontend logic

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,7 +1,57 @@
-const page = () => {
+import { currentUser } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+
+import Searchbar from '@/components/shareds/Searchbar'
+import Pagination from '@/components/shareds/Pagination'
+import CommunityCard from '@/components/cards/CommunityCard'
+
+import { fetchUser } from '@/lib/actions/user.actions'
+import { fetchCommunities } from '@/lib/actions/community.actions'
+
+async function Page({ searchParams }: { searchParams: { [key: string]: string | undefined } }) {
+  const user = await currentUser()
+  if (!user) return null
+
+  const userInfo = await fetchUser(user.id)
+  if (!userInfo?.onboarded) redirect('/onboarding')
+
+  const result = await fetchCommunities({
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
+    pageSize: 25,
+  })
+
   return (
-    <div>Community</div>
+    <>
+      <h1 className="head-text">Communities</h1>
+
+      <div className="mt-5">
+        <Searchbar routeType="communities" />
+      </div>
+
+      <section className="mt-9 flex flex-wrap gap-4">
+        {result.communities.length === 0 ? (
+          <p className="no-result">No Result</p>
+        ) : (
+          <>
+            {result.communities.map((community) => (
+              <CommunityCard
+                key={community.id}
+                id={community.id}
+                name={community.name}
+                username={community.username}
+                imgUrl={community.image}
+                bio={community.bio}
+                members={community.members}
+              />
+            ))}
+          </>
+        )}
+      </section>
+
+      <Pagination path="communities" pageNumber={searchParams?.page ? +searchParams.page : 1} isNext={result.isNext} />
+    </>
   )
 }
 
-export default page
+export default Page
